212401e148131e89374ebccca261acd67ae066e9
Philipp Daun <post@philippdaun.net>
2020-08-01 14:02:21 +0200

    Fix idle function default parameters

--- src/dom/on-idle-after-load.js
+++ src/dom/on-idle-after-load.js
@@ -1,7 +1,7 @@
 import onWindowLoad from './on-window-load'
 import onIdle from './on-idle'
 
-export default function onIdleAfterLoad(callback, { delayAfterLoad = 0 }) {
+export default function onIdleAfterLoad(callback, { delayAfterLoad = 0 } = {}) {
   onWindowLoad(() => {
     if (delayAfterLoad > 0) {
       setTimeout(() => onIdle(callback), delayAfterLoad)
--- src/dom/on-idle.js
+++ src/dom/on-idle.js
@@ -1,6 +1,6 @@
-export default function onIdle(callback, { timeoutFallback = 1000 }) {
+export default function onIdle(callback, { timeoutFallback = 1000 } = {}) {
   if ('requestIdleCallback' in window) {
-    requestIdleCallback(callback)
+    window.requestIdleCallback(callback)
   } else {
     setTimeout(callback, timeoutFallback)
   }
